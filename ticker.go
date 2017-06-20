dcaba8fc929971172c84445e4fe883d4c0a873b8
Paulo Quintans Pereira <paulo.quintans@gmail.com>
2017-06-20 21:15:09 +0100

    new ticker

--- ticker.go
+++ ticker.go
@@ -11,19 +11,28 @@ type Ticker struct {
 	ticker *time.Ticker
 }
 
-func NewTicker(duration time.Duration, hnd func(time.Time)) Ticker {
-	var tck = Ticker{
+func NewTicker(duration time.Duration, hnd func(time.Time)) *Ticker {
+	return NewDelayedTicker(duration, duration, hnd)
+}
+
+func NewDelayedTicker(delay time.Duration, duration time.Duration, hnd func(time.Time)) *Ticker {
+	var tck = &Ticker{
 		stop:   make(chan struct{}, 1),
 		ticker: time.NewTicker(duration),
 	}
 
 	go func() {
+		if delay > 0 {
+			time.Sleep(delay)
+		}
+		hnd(time.Now())
+
 		for {
 			select {
-			case t := <-tck.ticker.C:
-				hnd(t)
 			case <-tck.stop:
 				return
+			case t := <-tck.ticker.C:
+				hnd(t)
 			}
 		}
 	}()
@@ -31,7 +40,7 @@ func NewTicker(duration time.Duration, hnd func(time.Time)) Ticker {
 	return tck
 }
 
-func (tck Ticker) Stop() {
+func (tck *Ticker) Stop() {
 	tck.once.Do(func() {
 		close(tck.stop)
 		tck.ticker.Stop()
